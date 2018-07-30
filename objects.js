4f8df2f6214060efe3d04261fc24ad8caff4d551
griffen.bradds@gmail.com <griffen.bradds@gmail.com>
2018-07-30 00:33:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  playlist[artistName] = songTitle;
+  delete playlist[artistName];
   return playlist;
 }
