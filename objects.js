d731424ef78ad58cb62e62b44ea0696030040b68
griffen.bradds@gmail.com <griffen.bradds@gmail.com>
2018-07-30 00:26:40 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {artistName: "songTitle"};
+
+
+//function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) //{
+// obj[key] = value
+// 
+//  return obj
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
